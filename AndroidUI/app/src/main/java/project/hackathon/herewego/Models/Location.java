511dc8af63118d5983982cdbfee85d6ff8f40a06
Saikat Dutta <Saikat Dutta>
2016-07-24 16:01:21 +0530

    edittrip first commit

---
+++ AndroidUI/app/src/main/java/project/hackathon/herewego/Models/Location.java
@@ -0,0 +1,14 @@
+package project.hackathon.herewego.Models;
+
+/**
+ * Created by saikat on 7/24/2016.
+ */
+public class Location {
+
+    String Id;
+    double Latitude;
+    double Longitude;
+    float rating;
+    String category;
+    boolean IsPreferred;
+}
